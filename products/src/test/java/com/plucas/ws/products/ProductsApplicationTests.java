7ec86ccd501a9d36ec0925e44c6a7f6eadfa187e
plucas <pablo.lucv@gmail.com>
2024-02-15 16:22:08 -0600

    first commit initial files

---
+++ products/src/test/java/com/plucas/ws/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plucas.ws.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
